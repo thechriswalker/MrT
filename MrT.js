a7321b495ade1229118ffac65974ae4aa1b41344
Chris Walker <chris.walker@fantasyshopper.com>
2012-09-28 22:21:17 +0100

    Refactoring, basically playing golf
    Not seriously or it would be all 1 character variable names,
    but just applying some golf tactics and manuall minifying for
    a smaller, yet still vaguely readable script. Well, maybe I can read
    it because I wrote it... we'll see in 6 months...

--- MrT.js
+++ MrT.js
@@ -1,25 +1,14 @@
 /* Mr T - Javascript MicroTest Library, Fool. */
-var MrT = (function(d){
-  var list = d.createElement('ol'), totals = d.createElement('p'), style = d.createElement('style'),
-    counts = [0,0,0], total = function(done){
-      totals.innerText = counts.slice(0,2).join("/");
-      if(done!==void(0)){ totals.innerText += done?" PASS":" FAIL!"; totals.className = done?'p':'f'; }
-    };
-    style.innerText = '.p,.f{margin:1px;border-left:1em solid #0a0;padding-left:0.5em;}.f{color:#c00;border-color:#c00;}';
-  return function test(name, func){
-    var called = false, li = d.createElement('li'), complete = function(ok){
-      return function(){
-        if(called){ return; } called = true;
-        li.innerHTML = name; li.className = ok?'p':'f';
-        if(ok){ counts[0]++; } counts[1]++;
-        list.appendChild(li); total();
-        setTimeout(function(){
-          if(counts[2]===counts[1]){ total(counts[0]===counts[1]); }
-        },100);
-      };
-    }, promise = { pass: complete(true), fail: complete(false) };
-    if(counts[2] === 0){ [style,totals,list].forEach(function(v){ d.body.appendChild(v); }); }
-    counts[2]++;
-    func(promise);
-  };
-})(document);
+var MrT = (function(d, ac, ce, it, id, p){
+var b=d.body,list=d[ce]('ul'),_=list.id=id,totals=d[ce]('p'),style=d[ce]('style'),counts=[0,0,0],
+total=function(done){totals[it]=counts.slice(0,2).join("/");totals.className=done?'p':'f';};
+style[it]=['ul{list-style-type:none;}li'+p+'none;}ul:target '+p+'block}.p,.f{position:relative;}.p',
+'✔";color:#0a0;}.f:','✘";color:#900;}p.p,p.f{margin-left:20px;}'].join(':before {left:-20px;position:absolute;content:"');
+return function test(name, func){
+  var called=false,li=d[ce]('li'),e=function(ok){return function(){
+  if(called||!(called=true)){return;}
+  list.appendChild(li,li.innerHTML=name,li.className=ok?'p':'f');
+  if(++counts[1] && ok){ counts[0]++; }
+  total(setTimeout(function(){if(counts[2]===counts[1]){ total(counts[0]===counts[1]);}},0));
+  };}; if(!counts[2]++ || [style,totals,list].forEach(function(v){b[ac](v);})||true){func({pass:e(!0),fail:e(!1)});}
+};})(document,'appendChild','createElement','innerText','show_passes','.p{display:');
